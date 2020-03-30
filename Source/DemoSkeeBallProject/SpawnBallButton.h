2501192dcf8c922c0274a06cb76eb82d78c41428
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2020-03-30 18:09:29 -0400

    Created SkeeBall Pickup & Spawn Ball Button. Fixed Score representation.

---
+++ Source/DemoSkeeBallProject/SpawnBallButton.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRBase/AWorldButton/AWorldButton.h"
+#include "SpawnBallButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOSKEEBALLPROJECT_API ASpawnBallButton : public AWorldButton
+{
+	GENERATED_BODY()
+
+public:
+
+	
+	
+	
+
+
+
+	
+};
